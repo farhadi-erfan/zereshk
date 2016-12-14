9b79beaa42aa0b96c90ced4652dcb7f2cbbb11f4
Erfan Farhadi <Erfan Farhadi>
2016-12-14 17:15:43 +0330

    erfan ferfere! update!

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
+int multiple (int a, int b){
+		return a * b;
+}
 
 int main () {
-	printf("Hello!");
+	int n , m;
+	scanf("%d%d", &n, &m);
+	printf("%d", multiple(n, m));
 	return 0;
 }
